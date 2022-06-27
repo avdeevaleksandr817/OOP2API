37b3f260a19dfb652008e010b722cf2d0bc902a4
avdeevaleksandr <avdeevaleksandr817@gmail.com>
2022-06-27 21:18:12 +0400

    first

---
+++ src/main/java/Post.java
@@ -0,0 +1,9 @@
+public class Post {
+    public FormDate birthday;
+    public String name;
+    public String patronymic;
+    public String surname;
+    public String passport;
+    public String phone;
+    public boolean subscription;
+}
